2a4f78a8565cb5be535b49563a96293bf2150468
Tanishq Homkar <tanishqhomkart7@gmail.com>
2025-05-17 13:31:17 +0530

    Extract Features
    New file for extracting features. Used MFCC.

---
+++ features/extract_features.py
@@ -0,0 +1,49 @@
+import numpy as np
+import librosa
+
+def compute_mfcc(
+        signal: np.ndarray,
+        sr: int,
+        n_mfcc: int = 13,
+        n_fft: int = 2048,
+        hop_length: int = 512) -> np.ndarray:
+    
+    if signal.dtype != np.float32:
+        signal = signal.astype(np.float32)
+        signal = signal / np.max(np.abs(signal))
+    
+    mfcc = librosa.feature.mfcc(y=signal,
+                                sr=sr,
+                                n_mfcc=n_mfcc,
+                                hop_length=hop_length,
+                                n_fft=n_fft)
+    
+    # mfcc = librosa.util.normalize(mfcc, axis=1)
+    return mfcc
+
+
+def sliding_window_feature(signal: np.ndarray,
+                           sr: int,
+                           window_sec: float = 1.0,
+                           hop_sec: float = 0.5,
+                           feature_fn = compute_mfcc,
+                           **feat_kwargs) -> tuple[np.ndarray, np.ndarray]:
+    
+    window_len = int(window_sec * sr)
+    hop_len = int(hop_sec * sr)
+
+    features = []
+    centers = []
+
+    for start in range(0, len(signal) - window_len+1, hop_len):
+        end = start + window_len
+        seg = signal[start:end]
+        feat = feature_fn(seg, sr, **feat_kwargs)
+        features.append(feat)
+        centers.append((start + window_len / 2) / sr)
+
+    return features, np.array(centers)
+
+
+def aggregate_segment_mfcc(mfcc: np.ndarray) -> np.ndarray:
+    return np.mean(mfcc, axis=1)
--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from features.audio_utils import (
     extract_segments,
     input_timecodes_from_user
 )
+from features.extract_features import(
+    compute_mfcc,
+    sliding_window_feature,
+    aggregate_segment_mfcc
+)
 from visualization.plot_waveform import plot_waveform
 from pydub import AudioSegment
 
@@ -36,6 +41,20 @@ def main():
 
     anomaly_clips = extract_segments(signal, sr, timecodes)
     print(f"[INFO] Extracted {len(anomaly_clips)} anomaly segments.")
+
+    anomaly_feats = []
+    for i, clips in enumerate(anomaly_clips, start=1):
+        mfcc = compute_mfcc(clips, sr)
+        agg = aggregate_segment_mfcc(mfcc)
+        anomaly_feats.append(agg)
+        print(f"[INFO] Anomaly Clip {i}: MFCC Matrix {mfcc.shape}, aggregated vector {agg.shape}.")
+
+
+    window_sec, hop_sec = 1.0, 0.5
+    full_feats, centers = sliding_window_feature(signal, sr, window_sec=window_sec, hop_sec=hop_sec)
+
+    print(f"[INFO] Full audio sliced into {len(full_feats)} windows.")
+    print(f"[INFO] First window MFCC shape: {full_feats[0].shape}, center at {centers[0]:.2f}s")
 
     os.makedirs(args.output_dir, exist_ok=True)
     base_name = os.path.splitext(os.path.basename(args.input))[0]
